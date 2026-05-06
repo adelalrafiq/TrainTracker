9a5eeaab2b6f39d26a7354c1d9380d1494ca7c46
adelalrafiq <adelalrafiq@gmail.com>
2026-05-06 10:33:20 +0200

    Change Cors and Swagger

--- TrainTracker.Api/Models/DTOs/LiveboardRowDto.cs
+++ TrainTracker.Api/Models/DTOs/LiveboardRowDto.cs
@@ -2,10 +2,15 @@
 
 public class LiveboardRowDto
 {
+  /// <example>Antwerp-Central</example>
   public string DirectionName { get; set; }
+  /// <example>2026-05-04T08:30:00</example>
   public DateTimeOffset DepartureTime { get; set; }
+  /// <example>4</example>
   public string Platform { get; set; }
+  /// <example>IC 1830</example>
   public string VehicleInfoShortname { get; set; }
+  /// <example>5</example>
   public int DelayMinutes { get; set; }
   public TrainStatus Status { get; set; }
 }
--- TrainTracker.Api/Program.cs
+++ TrainTracker.Api/Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using TrainTracker.Api.Data;
+using Microsoft.OpenApi;
 using TrainTracker.Api.Services.Implementations;
 using TrainTracker.Api.Services.Interfaces;
 
@@ -9,17 +8,37 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddMemoryCache();
 builder.Services.AddSignalR();
 builder.Services.AddHttpClient<ILiveboardService, LiveboardService>();
 builder.Services.AddScoped<IStationsService, StationsService>();
+
+// Swagger/OpenAPI configuration
+builder.Services.AddSwaggerGen(options =>
+{
+  options.SwaggerDoc("v1", new OpenApiInfo
+  {
+    Title = "🚆 TrainTracker API",
+    Version = "v1",
+    Description = "Real-time train departure board API using iRail data.",
+    Contact = new OpenApiContact
+    {
+      Name = "Adel Al-Rafiq",
+      Email = "adelalrafiq@gmail.com"
+    }
+  });
+});
+
 //builder.Services.AddCors();
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins")
+    .Get<string[]>();
+
 builder.Services.AddCors(options =>
 {
   options.AddPolicy("AllowAngularClient", policy =>
   {
-    policy.WithOrigins("http://localhost:4200")
+    policy.WithOrigins(allowedOrigins)
           .AllowAnyHeader()
           .AllowAnyMethod()
           .AllowCredentials();
@@ -27,11 +46,6 @@ builder.Services.AddCors(options =>
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(options =>
-  options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
 
 var app = builder.Build();
 
@@ -39,10 +53,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
   app.MapOpenApi();
-  app.UseSwagger();
-  app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+  c.InjectStylesheet("/swagger-ui/custom.css");
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "TrainTracker API v1");
+  c.DocumentTitle = "TrainTracker API Docs";
+  c.RoutePrefix = "swagger";
+});
+
 app.UseCors("AllowAngularClient");
 app.UseAuthorization();
 
